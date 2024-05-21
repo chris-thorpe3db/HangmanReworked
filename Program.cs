30ac12eba64699e5f810ce64779d91f480d41ccb
Christopher Thorpe <drummer1074spam@gmail.com>
2024-05-20 21:44:07 -0400

    Corrected a stupid whitespace mistake

--- Program.cs
+++ Program.cs
@@ -134,8 +134,7 @@ namespace HangmanReworked {
                 if (consoleInput == "show c")
                 {
                     // Show GNU GPL 3.0 Copyright/Distribution Information
-                    Console.WriteLine(
-                        "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. \nFor more information, visit https://www.gnu.org/licenses/. \n\nPress any key to continue.");
+                    Console.WriteLine("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. \nFor more information, visit https://www.gnu.org/licenses/. \n\nPress any key to continue.");
                     Console.ReadKey();
                     Console.Clear();
                     continue;
